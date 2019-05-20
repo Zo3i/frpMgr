87e69750cd37efde56ae3ede60daf8f5662a01a4
Zo3i <30296801+Zo3i@users.noreply.github.com>
2019-05-20 13:03:24 +0800

    fix

--- web/src/main/docker/final/run.sh
+++ web/src/main/docker/final/run.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
+rm -rf ./frpMgr
 #down file
 https://github.com/Zo3i/frpMgr.git
 #enter path
 cd ./frpMgr/web/src/main/docker/final
 chmod -R 755 ./*
+cd ./mysql
 docker build -t jo/mysql .
+cd ..
 docker-compose build
 docker-compose up -d
