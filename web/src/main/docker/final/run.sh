4cf35ae89e9b6c95b3843c9414dd56633a2ff5c5
Zo3i <30296801+Zo3i@users.noreply.github.com>
2019-06-21 09:42:57 +0800

    update 一键安装

--- web/src/main/docker/final/run.sh
+++ web/src/main/docker/final/run.sh
@@ -1,11 +1,43 @@
 #!/bin/sh
 rm -rf ./frpMgr
 
+getOsName()
+{
+    if grep -Eqii "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
+        DISTRO='CentOS'
+        PM='yum'
+    elif grep -Eqi "Red Hat Enterprise Linux Server" /etc/issue || grep -Eq "Red Hat Enterprise Linux Server" /etc/*-release; then
+        DISTRO='RHEL'
+        PM='yum'
+    elif grep -Eqi "Aliyun" /etc/issue || grep -Eq "Aliyun" /etc/*-release; then
+        DISTRO='Aliyun'
+        PM='yum'
+    elif grep -Eqi "Fedora" /etc/issue || grep -Eq "Fedora" /etc/*-release; then
+        DISTRO='Fedora'
+        PM='yum'
+    elif grep -Eqi "Debian" /etc/issue || grep -Eq "Debian" /etc/*-release; then
+        DISTRO='Debian'
+        PM='apt'
+    elif grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release; then
+        DISTRO='Ubuntu'
+        PM='apt'
+    elif grep -Eqi "Raspbian" /etc/issue || grep -Eq "Raspbian" /etc/*-release; then
+        DISTRO='Raspbian'
+        PM='apt'
+    else
+        DISTRO='unknow'
+    fi
+    echo $DISTRO;
+}
+
 if command -v git >/dev/null 2>&1; then
   echo 'exists'
 else
   echo 'no exists'
-  yum install -y git
+  if [ getOsName = 'CentOs' ];
+  then yum install -y git
+  if [ getOsName = 'Debian' ];
+    then apt-get install git
 fi
 
 #down file
@@ -16,5 +48,5 @@ chmod -R 755 ./*
 cd ./mysql
 docker build -t jo/mysql .
 cd ..
-docker-compose build
-docker-compose up -d
+sudo docker-compose build
+sudo docker-compose up -d
