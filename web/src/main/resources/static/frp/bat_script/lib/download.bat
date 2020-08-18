9b15973f295b9126b99c001a927ca62ed114f1a7
Zo3i <30296801+Zo3i@users.noreply.github.com>
2020-08-18 16:26:16 +0800

    升级bat 下载函数使用power shell下载文件

--- web/src/main/resources/static/frp/bat_script/lib/download.bat
+++ web/src/main/resources/static/frp/bat_script/lib/download.bat
@@ -26,7 +26,7 @@ set fileName=frpc.ini
 
 if %useFor%==1 (
   echo 下载WEB穿透配置文件...
-  call :downloadFunc %downloadWEBUrl% %fileName%
+  call :downFunc %downloadWEBUrl% %fileName%
   echo.
 ) else if %useFor%==2 (
   echo 下载文件目录配置文件...
@@ -38,24 +38,23 @@ if %useFor%==1 (
     md file
   )
 
-  call :downloadFunc %downloadFILEUrl% %fileName%
+  call :downFunc %downloadFILEUrl% %fileName%
   echo.
 ) else (
   echo 下载远程桌面配置文件...
-  call :downloadFunc %downloadRDPUrl% %fileName%
+  call :downFunc %downloadRDPUrl% %fileName%
   echo.
 )
 
 :: 下载
 set downloadFrpConfigUrl=%host%/FULL_INI
 set frpFullName=frpc_full.ini
-call :downloadFunc %downloadFrpConfigUrl% %frpFullName%
+call :downFunc %downloadFrpConfigUrl% %frpFullName%
 
 set downloadExeUrl=%host%/EXE
 set frpExeName=frpc.exe
-
 if not exist .\client\frpc.exe (
-  call :downloadFunc %downloadExeUrl% %frpExeName%
+  call :downFunc %downloadExeUrl% %frpExeName%
 )
 
 goto:EXIT
@@ -63,30 +62,7 @@ goto:EXIT
 
 ::::::::::::::::::::函数::::::::::::::::::::
 :: 下载函数
-:downloadFunc
-  set Save=.\client
-  if exist %Save% (echo 位置：%Save%) else (mkdir %Save% & echo 已创建：%Save%)
+:downFunc
   set Url=%1
   set SaveName=%2
-  for %%a in ("%Url%") do set "FileName=%%~nxa"
-    echo 正在下载%FileName%，请勿关闭窗口...
-    if not defined Save set "Save=%cd%"
-    (echo Download Wscript.Arguments^(0^),Wscript.Arguments^(1^)
-    echo Sub Download^(url,target^)
-    echo   Const adTypeBinary = 1
-    echo   Const adSaveCreateOverWrite = 2
-    echo   Dim http,ado
-    echo   Set http = CreateObject^("Msxml2.ServerXMLHTTP"^)
-    echo   http.open "GET",url,False
-    echo   http.send
-    echo   Set ado = createobject^("Adodb.Stream"^)
-    echo   ado.Type = adTypeBinary
-    echo   ado.Open
-    echo   ado.Write http.responseBody
-    echo   ado.SaveToFile target
-    echo   ado.Close
-    echo End Sub)>DownloadFile.vbs
-    DownloadFile.vbs "%Url%" "%Save%\%SaveName%"
-    ::下载完删除生成的vbs文件
-    del DownloadFile.vbs
-:EXIT
+  (powershell "($client = new-object System.Net.WebClient) -and ($client.DownloadFile('%Url%', '.\client\%SaveName%')) -and (exit)") | cmd
